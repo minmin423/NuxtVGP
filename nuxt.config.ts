ee596d5f81341e7e14466c7f0b5fc2f905144854
mingming0919 <madelacruz423@gmail.com>
2024-03-06 18:32:36 +0800

    initial setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
-import vuetify from 'vite-plugin-vuetify'
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
 	srcDir: 'client/',
-	build: { transpile: ['vuetify'] },
+	// build: { transpile: ['vuetify'] },
 	imports: { dirs: ['./stores'] },
 	apollo: {
 		autoImports: true,
@@ -12,8 +9,9 @@ export default defineNuxtConfig({
 			default: { httpEndpoint: 'https://spacex-production.up.railway.app/' },
 		},
 	},
-	vite: {
-		plugins: [vuetify()],
-	},
-	modules: ['@nuxtjs/apollo', ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }]],
+	modules: [
+        '@nuxtjs/apollo',
+        ['@pinia/nuxt', { autoImports: ['defineStore', 'acceptHMRUpdate'] }],
+        "@nuxtjs/tailwindcss"
+    ],
 })
