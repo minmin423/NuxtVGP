3c7e1d1f7a061bf1875467f5cad88b304c919081
Dela Cruz, Michael Angelo <123302344+minmin423@users.noreply.github.com>
2024-03-08 10:17:13 +0800

    Update Header.vue

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -2,7 +2,7 @@
 	<nav class="flex align-center justify-between px-2 lg:px-10 py-5 font-semibold">
 		<h1 class="text-3xl font-bold">SpaceX</h1>
 
-		<div class="flex gap-4">
+		<div class="flex align-center gap-4">
 			<NuxtLink to="/">Home</NuxtLink>
 			<NuxtLink to="/rockets">Rockets</NuxtLink>
 			<NuxtLink to="/favorites">Favorites</NuxtLink>
